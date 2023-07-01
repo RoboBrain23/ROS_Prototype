421648ac7c470ec7ad8985a176e5fd260b43e54a
shehab-ashraf <ashrafshehab377@gmail.com>
2023-07-01 21:24:10 +0300

    delete unwanted files

--- build/move_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shehab/catkin_ws/src/move_robot
-# Build directory: /home/shehab/catkin_ws/build/move_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
